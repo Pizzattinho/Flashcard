8752513fbedbdb4f98945ba610c36b4fc04d07e9
Pizzattinho <paulo.pizzatto@escola.pr.gov.br>
2025-06-13 08:08:38 -0300

    ....

---
+++ perguntas.js
@@ -0,0 +1,99 @@
+criaCartao(
+  'História - Império Romano',
+   'Qual foi seu primeiro imperador?',
+   'Augusto')
+   
+criaCartao(
+  'História - Império Romano',
+  'Quando começou a reinar?',
+  '14 d.C.')
+   
+ criaCartao(
+  'História - Império Romano',
+  'Qual foi seu último imperador?',
+  'Rómulo Augústulo')
+   
+  criaCartao(
+  'História - Império Romano',
+  'Quando parou de reinar?',
+  '476 d.C.')
+   
+  criaCartao(
+  'História - Império Romano',
+  'Quando parou de existir?',
+  '476 d.C')
+   
+  criaCartao(
+  'Biologia - Animália',
+  'Qual caractéristica define um ser desse reino?',
+  'Poder se locomover,e ser heterotrófico')
+   
+    criaCartao(
+    'Biologia - Plantae',
+    'Qual caractéristica define um ser desse reino?',
+    'Capacidade de fotossíntese')
+   
+    criaCartao(
+    'Biologia - Fungi',
+    'Qual caractéristica define um ser desse reino?',
+    'São heterotrófico, e possuirem parede celular')
+   
+    criaCartao(
+    'Biologia - Monera',
+    'Qual caractéristica define um ser desse reino?',
+    ' Ser procarionte')
+   
+    criaCartao(
+    'Biologia - Protoctista',
+    'Qual caractéristica define um ser desse reino?',
+    'Inclui seres unicelulares e multicelulares, serem procariontes')
+   
+    criaCartao(
+    'Biologia - Genética',
+    'O que é um alelo?',
+    'Uma das diferentes formas de um gene')
+   
+    criaCartao(
+    'Matemática - Seno, Cosseno e Tangente',
+    'Qual é a definição de seno em um triângulo retângulo?',
+    'O seno de um ângulo é a razão entre o cateto oposto ao ângulo e a hipotenusa')
+   
+    criaCartao(
+    'Matemática - Seno, Cosseno e Tangente',
+    'Qual é a definição de cosseno em um triângulo retângulo?',
+    'O cosseno de um ângulo é a razão entre o cateto adjacente ao ângulo e a hipotenusa')
+   
+    criaCartao(
+    'Matemática - Seno, Cosseno e Tangente',
+    'Qual é a definição de tangente em um triângulo retângulo?',
+    'A tangente de um ângulo é a razão entre o cateto oposto ao ângulo e o cateto adjacente')
+   
+    criaCartao(
+    'Matemática - Seno, Cosseno e Tangente',
+    'Qual é a relação fundamental entre seno e cosseno em um triângulo retângulo?',
+    'Seno ao quadrado mais cosseno ao quadrado é igual a 1')
+   
+    criaCartao(
+    'Matemática - Seno, Cosseno e Tangente',
+    'Qual é a tangente de 45 graus?',
+    '1')
+   
+    criaCartao(
+    'Português - Romantismo',
+    'Qual é o marco inicial do Romantismo no Brasil?',
+    'A publicação de "Suspiros Poéticos e Saudades" de Gonçalves de Magalhães, em 1836')
+   
+    criaCartao(
+    'Português - Romantismo',
+    'Qual foi o principal autor da primeira geração romântica no Brasil?',
+    'Gonçalves Dias')
+   
+    criaCartao(
+    'Português - Romantismo',
+    'Qual característica do Romantismo é conhecida por valorizar o nacionalismo e o sentimentalismo?',
+    'Exaltação dos sentimentos e da pátria')
+   
+    criaCartao(
+    'Português - Romantismo',
+    'Em qual contexto histórico o Romantismo surgiu no Brasil?',
+    'Durante o período pós-independência do Brasil, no século XIX')
